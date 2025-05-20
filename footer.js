680e6246c9a2187b107f26dac1c43202049c631d
aromaric <aromaricadjei213@gmail.com>
2025-05-20 16:06:37 +0000

    JS total demo

---
+++ footer.js
@@ -0,0 +1,1 @@
+document.write("This page is Copyright" + new Date().getFullYear());
---
+++ script1.js
@@ -0,0 +1,9 @@
+<nosript>
+    <p>
+        Hey, your browser has JavaSript turned off !
+    </p>
+    <p>
+        Okay, cool, perhaps you'll prefer this <a href="no-js.html">
+        non-JavaScript version</a> of the page
+    </p>
+</nosript>
