4ee09f35a218b18373636ed5cd9d7f4ee188b1c2
aromaric <aromaricadjei213@gmail.com>
2025-05-22 12:33:19 +0000

    JS - Variables and Loops

--- script1.js
+++ script1.js
@@ -1,9 +1,9 @@
-<nosript>
-    <p>
-        Hey, your browser has JavaSript turned off !
-    </p>
-    <p>
-        Okay, cool, perhaps you'll prefer this <a href="no-js.html">
-        non-JavaScript version</a> of the page
-    </p>
-</nosript>
+let sum = 0;
+let num;
+do {
+    num = prompt("Type a number; when you're done," +
+        "click Cancel:", 1);
+    sum += Number(num);
+}
+while (num !== null || sum ===0);
+document.writeln("The total of your numbers is" + num);
