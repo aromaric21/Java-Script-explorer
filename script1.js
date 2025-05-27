19e0016b80152c1631b3b73fcd62d479236775e2
aromaric <aromaricadjei213@gmail.com>
2025-05-27 12:40:37 +0000

    arrow function implicit retrun

--- script1.js
+++ script1.js
@@ -1,11 +1,8 @@
 const preTipTotal = 100.00;
 const tipPercentage = 0.15;
 
-// Declare tipCost using an arrow function
-const tipCost = (preTip, tipPercent) => {
-    const tipResult = preTip * tipPercent;
-    return (tipResult);
-}
+// Declare tipCost using an arrow function implicit retrun
+const tipCost = (preTip, tipPercent) => preTip * tipPercent;
 
 const totalBill = preTipTotal + tipCost(preTipTotal, tipPercentage);
 document.write("Your total bill is $" + totalBill);
