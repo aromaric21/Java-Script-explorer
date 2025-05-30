d7dd058f1f4ae40189d11314d55c1b1b0ebe94a4
aromaric <aromaricadjei213@gmail.com>
2025-05-30 18:55:17 +0000

    Handle String in JS

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
-console.log('Happy developing ✨')
+function myFunction() {
+    return "filename.html";
+}
+
+const bookName = "JavaScript Essentials for Dummies";
+
+length1 = myFunction().length;
+length2 = bookName.length;
+length3 = "123\n5678".length;
+
+console.log(length1);
+console.log(length2);
+console.log(length3);
 
--- script1.js
+++ script1.js
@@ -6,3 +6,21 @@ const tipCost = (preTip, tipPercent) => preTip * tipPercent;
 
 const totalBill = preTipTotal + tipCost(preTipTotal, tipPercentage);
 document.write("Your total bill is $" + totalBill);
+
+
+
+// String Object Methods for Searching for Substrings
+// string.endsWith(substring, start)
+// string.includes(substring, start)
+// string.indexOf(substring, start)
+// string.lastIndexOf(substring, start)
+// string.startsWith(substring, start)
+
+
+// String Object Methods for Extracting Substrings
+// string.charAt(index)
+// string.charCodeAt(index)
+// string.slice(start, end)
+// string.split(separator, limit)
+// string.substr(start, length)
+// string.substring(start, end)
