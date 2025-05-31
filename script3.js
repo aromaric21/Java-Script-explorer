6c31a6e22015bcc042866a7caf9b3796d2b5ecc5
aromaric <aromaricadjei213@gmail.com>
2025-05-31 00:47:24 +0000

    Debugging JavaScript

---
+++ script3.js
@@ -0,0 +1,7 @@
+// Debugging JavaScript
+// Debugging errors using the Console
+// Setting breakpoints
+// Stepping through JavaScript code
+
+console.log("Hello World");
+console.table("Hello World");
